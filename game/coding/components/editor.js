5d88e5ec9ccf5cac8aba6300efc0b9743aa62dec
Raptor4 <raptor4mc@gmail.com>
2025-12-27 12:00:36 +0100

    Update editor.js

--- game/coding/components/editor.js
+++ game/coding/components/editor.js
@@ -1,1 +1,21 @@
 
+function initEditor() {
+    monaco.languages.register({ id: 'multiraptor' });
+    monaco.languages.setMonarchTokensProvider('multiraptor', {
+        tokenizer: {
+            root: [
+                [/\b(var|if|else|for|while|func|print|return)\b/, 'keyword'],
+                [/\b\d+/, 'number'],
+                [/"([^"\\$|\\.)*$/, 'string.invalid'],
+                [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+                [/[{}()\$\$]/, '@brackets'],
+                [/[a-z_$][\w$]*/, 'identifier'],
+            ],
+            string: [
+                [/[^\\"]+/, 'string'],
+                [/\\./, 'string.escape'],
+                [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+            ]
+        }
+    });
+}
