7492c4cea1f0978c8b60938754bd7d2a65f9da03
Raptor4 <raptor4mc@gmail.com>
2025-12-28 00:56:43 +0100

    Update engine.js

--- game/singleplayer/core/engine.js
+++ game/singleplayer/core/engine.js
@@ -1,1 +1,23 @@
+import { Renderer } from "./renderer.js";
+import { Input } from "./input.js";
+import { Tick } from "./tick.js";
 
+export class Engine {
+  constructor(canvas, world) {
+    this.renderer = new Renderer(canvas, world);
+    this.input = new Input();
+    this.tick = new Tick(world);
+    this.running = false;
+  }
+
+  start() {
+    this.running = true;
+    const loop = () => {
+      if (!this.running) return;
+      this.tick.update();
+      this.renderer.render();
+      requestAnimationFrame(loop);
+    };
+    loop();
+  }
+}
