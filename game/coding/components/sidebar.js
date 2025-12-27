f9a6409e1dd3e7cb0f6f3a1d1b3853799b61e20e
Raptor4 <raptor4mc@gmail.com>
2025-12-27 12:00:20 +0100

    Update sidebar.js

--- game/coding/components/sidebar.js
+++ game/coding/components/sidebar.js
@@ -1,1 +1,7 @@
-
+function initSidebar() {
+    document.getElementById('search-files').oninput = (e) => {
+        const query = e.target.value;
+        const files = Object.keys(window.multiRaptorCurrentRepo.files).filter(f => f.includes(query));
+        document.getElementById('files-list').innerHTML = files.map(f => `<div class="file-item repo-item" onclick="openFile('${f}')">${f}</div>`).join('');
+    };
+}
