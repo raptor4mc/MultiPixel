e422891910f5b98c17be6ab930410817b92e1808
Raptor4 <raptor4mc@gmail.com>
2025-12-28 00:57:27 +0100

    Update chunks.js

--- game/singleplayer/core/chunks.js
+++ game/singleplayer/core/chunks.js
@@ -1,1 +1,17 @@
+import { generateChunk } from "../worldgen/generator.js";
 
+export class ChunkManager {
+  constructor(config) {
+    this.seed = config.seed;
+    this.terrain = config.terrain;
+    this.chunks = new Map();
+  }
+
+  getChunk(x, z) {
+    const key = `${x},${z}`;
+    if (!this.chunks.has(key)) {
+      this.chunks.set(key, generateChunk(x, z, this.seed, this.terrain));
+    }
+    return this.chunks.get(key);
+  }
+}
