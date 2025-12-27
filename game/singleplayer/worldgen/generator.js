0d4635278a40e9fd6cc4ebe1fe5bc182173d5da2
Raptor4 <raptor4mc@gmail.com>
2025-12-28 00:57:51 +0100

    Update generator.js

--- game/singleplayer/worldgen/generator.js
+++ game/singleplayer/worldgen/generator.js
@@ -1,1 +1,7 @@
+import { flat } from "./flat.js";
+import { normal } from "./normal.js";
 
+export function generateChunk(cx, cz, seed, type) {
+  if (type === "flat") return flat(cx, cz);
+  return normal(cx, cz, seed);
+}
