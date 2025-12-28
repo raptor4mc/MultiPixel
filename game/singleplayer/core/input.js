be1e0931dc70acdc466cf2e197e5eb5e35655ea9
Raptor4 <raptor4mc@gmail.com>
2025-12-28 21:20:45 +0100

    Update input.js

--- game/singleplayer/core/input.js
+++ game/singleplayer/core/input.js
@@ -1,34 +1,38 @@
 export class Input {
-  constructor(canvas) {
-    this.keys = new Set();
-    this.mouseDX = 0;
-    this.locked = false;
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.keys = {};
+        this.mouseDX = 0;
+        this.mouseDY = 0;
+        this.locked = false;
 
-    window.addEventListener("keydown", e => this.keys.add(e.code));
-    window.addEventListener("keyup", e => this.keys.delete(e.code));
+        window.addEventListener("keydown", e => this.keys[e.code] = true);
+        window.addEventListener("keyup", e => this.keys[e.code] = false);
 
-    canvas.addEventListener("click", () => {
-      canvas.requestPointerLock();
-    });
+        canvas.addEventListener("click", () => {
+            canvas.requestPointerLock();
+        });
 
-    document.addEventListener("pointerlockchange", () => {
-      this.locked = document.pointerLockElement === canvas;
-    });
+        document.addEventListener("pointerlockchange", () => {
+            this.locked = document.pointerLockElement === canvas;
+        });
 
-    document.addEventListener("mousemove", e => {
-      if (!this.locked) return;
-      this.mouseDX += e.movementX;
-    });
-  }
+        document.addEventListener("mousemove", e => {
+            if (!this.locked) return;
+            this.mouseDX += e.movementX;
+            this.mouseDY += e.movementY;
+        });
+    }
 
-  isDown(key) {
-    return this.keys.has(key);
-  }
+    consumeMouse() {
+        const dx = this.mouseDX;
+        const dy = this.mouseDY;
+        this.mouseDX = 0;
+        this.mouseDY = 0;
+        return { dx, dy };
+    }
 
-  consumeMouseDX() {
-    const dx = this.mouseDX;
-    this.mouseDX = 0;
-    return dx;
-  }
+    isDown(code) {
+        return !!this.keys[code];
+    }
 }
-
