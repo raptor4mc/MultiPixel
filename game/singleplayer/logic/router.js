cab2b5d5c0dc10652875d69c1788b4f083c2cbc5
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:34:37 +0100

    Update router.js

--- game/singleplayer/logic/router.js
+++ game/singleplayer/logic/router.js
@@ -1,9 +1,4 @@
-export function routeToGame(settings) {
-  sessionStorage.setItem(
-    "pendingWorld",
-    JSON.stringify(settings)
-  );
-
-  // This is where your big voxel engine HTML goes
+export function routeToEngine(settings) {
+  sessionStorage.setItem("worldSettings", JSON.stringify(settings));
   window.location.href = "../engine/engine.html";
 }
