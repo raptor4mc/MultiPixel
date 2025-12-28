0a69d3feb0f76f1303e5f4bebe974cdd8788a66e
Raptor4 <raptor4mc@gmail.com>
2025-12-28 15:22:11 +0100

    Update engine.js

--- game/singleplayer/core/engine.js
+++ game/singleplayer/core/engine.js
@@ -5,11 +5,9 @@ import { Tick } from "./tick.js";
 export class Engine {
   constructor(canvas, world) {
     this.world = world;
-
-    this.input = new Input(canvas);
     this.renderer = new Renderer(canvas, world);
+    this.input = new Input(canvas);
     this.tick = new Tick(world);
-
     this.running = false;
   }
 
@@ -17,19 +15,12 @@ export class Engine {
     this.running = true;
     let last = performance.now();
 
-    const loop = (now) => {
-      if (!this.running) return;
-
-      const dt = Math.min((now - last) / 1000, 0.05);
+    const loop = now => {
+      const dt = (now - last) / 1000;
       last = now;
 
-      // update player (movement + camera)
-      this.world.player.update(dt, this.input);
-
-      // update world systems
+      this.world.player.update(dt, this.input, this.world);
       this.tick.update(dt);
-
-      // render frame
       this.renderer.render();
 
       requestAnimationFrame(loop);
@@ -37,8 +28,5 @@ export class Engine {
 
     requestAnimationFrame(loop);
   }
-
-  stop() {
-    this.running = false;
-  }
 }
+
