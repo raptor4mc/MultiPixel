2d099fa1e2696c8c403f6fa1272c6f9b82ddf3f9
Raptor4 <raptor4mc@gmail.com>
2025-12-28 14:47:18 +0100

    Update world.js

--- game/singleplayer/core/world.js
+++ game/singleplayer/core/world.js
@@ -4,7 +4,8 @@ import { Player } from "./player.js";
 export class World {
   constructor(config) {
     this.config = config;
+
     this.player = new Player();
-    this.chunks = new ChunkManager(config);
+    this.chunks = new ChunkManager(this);
   }
 }
