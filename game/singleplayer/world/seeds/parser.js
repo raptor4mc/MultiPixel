f12387724878f929f2f73a44e2be1da8181f1f45
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:28:52 +0100

    Create parser.js

---
+++ game/singleplayer/world/seeds/parser.js
@@ -0,0 +1,8 @@
+export function parseSeed(input) {
+  let hash = 0;
+  for (let i = 0; i < input.length; i++) {
+    hash = (hash << 5) - hash + input.charCodeAt(i);
+    hash |= 0;
+  }
+  return Math.abs(hash);
+}
