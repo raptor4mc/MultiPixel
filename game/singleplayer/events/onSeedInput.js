4f00513391390ff4d5f2dbd88dc1a19f989dd9e1
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:35:44 +0100

    Update onSeedInput.js

--- game/singleplayer/events/onSeedInput.js
+++ game/singleplayer/events/onSeedInput.js
@@ -1,5 +1,3 @@
-window.customSeedInput = "";
-
-document.getElementById("seedInput").addEventListener("input", e => {
-  window.customSeedInput = e.target.value;
-});
+document.getElementById("seedInput").oninput = e => {
+  window.appState.seed = e.target.value || null;
+};
