f8b891897f63548422d84f8a37bfc8222c2bb63d
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:35:58 +0100

    Update onCreateWorld.js

--- game/singleplayer/events/onCreateWorld.js
+++ game/singleplayer/events/onCreateWorld.js
@@ -1,14 +1,3 @@
-import { buildSettings } from "../logic/settings.js";
-import { validateSettings } from "../logic/validation.js";
-import { routeToGame } from "../logic/router.js";
-
-document.getElementById("createWorld").addEventListener("click", () => {
-  const settings = buildSettings();
-
-  if (!validateSettings(settings)) {
-    alert("Invalid world settings");
-    return;
-  }
-
-  routeToGame(settings);
-});
+document.getElementById("worldName").oninput = e => {
+  window.appState.worldName = e.target.value;
+};
