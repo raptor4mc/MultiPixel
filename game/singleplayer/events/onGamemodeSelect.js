39b16f41268998c423ba683c26fca924cd520adb
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:55:11 +0100

    Update onGamemodeSelect.js

--- game/singleplayer/events/onGamemodeSelect.js
+++ game/singleplayer/events/onGamemodeSelect.js
@@ -1,10 +1,16 @@
-import gamemodes from "../data/gamemodes.json" assert { type: "json" };
 import { Button } from "../ui/components/button.js";
 
 const root = document.getElementById("gamemodeSelect");
 
-gamemodes.forEach(g => {
-  root.appendChild(
-    Button(g.name, () => window.appState.gamemode = g.id)
-  );
-});
+fetch("../data/gamemodes.json")
+  .then(res => res.json())
+  .then(gamemodes => {
+    gamemodes.forEach(g => {
+      root.appendChild(
+        Button(g.name, () => {
+          window.appState.gamemode = g.id;
+        })
+      );
+    });
+  })
+  .catch(err => console.error("Failed to load gamemodes:", err));
