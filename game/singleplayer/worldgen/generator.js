8c7c935be6905cc6273259fe2e67df5c7eea2e18
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:25:22 +0100

    Delete game/singleplayer/worldgen directory

--- game/singleplayer/worldgen/generator.js
+++
@@ -1,7 +0,0 @@
-import { flat } from "./flat.js";
-import { normal } from "./normal.js";
-
-export function generateChunk(cx, cz, seed, type) {
-  if (type === "flat") return flat(cx, cz);
-  return normal(cx, cz, seed);
-}
--- game/singleplayer/worldgen/noise.js
+++
@@ -1,5 +0,0 @@
-export function noise2D(x, z, seed) {
-  let n = x * 374761 + z * 668265 + seed * 144665;
-  n = (n ^ (n >> 13)) * 1274126177;
-  return ((n ^ (n >> 16)) >>> 0) / 4294967295;
-}
