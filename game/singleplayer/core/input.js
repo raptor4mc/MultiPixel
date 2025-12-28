d5243ffcddc91736d61982b80a52320520c6a396
Raptor4 <raptor4mc@gmail.com>
2025-12-28 01:05:10 +0100

    Update input.js

--- game/singleplayer/core/input.js
+++ game/singleplayer/core/input.js
@@ -1,1 +1,12 @@
+export class Input {
+  constructor() {
+    this.keys = new Set();
 
+    window.addEventListener("keydown", e => this.keys.add(e.code));
+    window.addEventListener("keyup", e => this.keys.delete(e.code));
+  }
+
+  isDown(key) {
+    return this.keys.has(key);
+  }
+}
