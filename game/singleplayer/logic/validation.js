1bdd45fe728a20204de98ef4919b91973831f8c5
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:34:23 +0100

    Update validation.js

--- game/singleplayer/logic/validation.js
+++ game/singleplayer/logic/validation.js
@@ -1,7 +1,5 @@
 export function validateSettings(settings) {
-  if (!settings.name) return false;
-  if (!settings.seed && settings.seed !== 0) return false;
-  if (!settings.gamemode) return false;
-  if (!settings.terrain) return false;
-  return true;
+  if (!settings.name) throw "World needs a name";
+  if (!settings.gamemode) throw "Gamemode missing";
+  if (!settings.terrain) throw "Terrain missing";
 }
