329e3020e127809ed3b8f3fffe05c7a5ea081535
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:53:31 +0100

    Update onTerrainSelect.js

--- game/singleplayer/events/onTerrainSelect.js
+++ game/singleplayer/events/onTerrainSelect.js
@@ -1,10 +1,13 @@
-import terrains from "../data/terrainTypes.json" assert { type: "json" };
 import { Button } from "../ui/components/button.js";
 
 const root = document.getElementById("terrainSelect");
 
-terrains.forEach(t => {
-  root.appendChild(
-    Button(t.name, () => window.appState.terrain = t.id)
-  );
-});
+fetch("../data/terrainTypes.json")
+  .then(res => res.json())
+  .then(terrains => {
+    terrains.forEach(t => {
+      root.appendChild(
+        Button(t.name, () => window.appState.terrain = t.id)
+      );
+    });
+  });
