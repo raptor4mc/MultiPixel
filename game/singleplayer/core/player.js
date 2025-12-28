b24dad667195fed776ebcd110cca5168b2a3be26
Raptor4 <raptor4mc@gmail.com>
2025-12-28 01:20:34 +0100

    Update player.js

--- game/singleplayer/core/player.js
+++ game/singleplayer/core/player.js
@@ -1,26 +1,26 @@
 export class Player {
   constructor() {
-    this.position = { x: 0, y: 40, z: 0 };
-    this.velocity = { x: 0, y: 0, z: 0 };
-
-    this.yaw = 0;
-    this.pitch = 0;
-
-    this.speed = 5;
-    this.gravity = -20;
-    this.onGround = false;
+    this.position = { x: 0, z: 0 };
+    this.speed = 6;
   }
 
   update(dt, input) {
-    if (input?.isDown("KeyW")) this.velocity.z -= this.speed * dt;
-    if (input?.isDown("KeyS")) this.velocity.z += this.speed * dt;
-    if (input?.isDown("KeyA")) this.velocity.x -= this.speed * dt;
-    if (input?.isDown("KeyD")) this.velocity.x += this.speed * dt;
+    let dx = 0;
+    let dz = 0;
 
-    this.velocity.y += this.gravity * dt;
+    if (input.isDown("KeyW")) dz -= 1;
+    if (input.isDown("KeyS")) dz += 1;
+    if (input.isDown("KeyA")) dx -= 1;
+    if (input.isDown("KeyD")) dx += 1;
 
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    this.position.z += this.velocity.z;
+    // normalize diagonal movement
+    if (dx !== 0 || dz !== 0) {
+      const len = Math.hypot(dx, dz);
+      dx /= len;
+      dz /= len;
+    }
+
+    this.position.x += dx * this.speed * dt;
+    this.position.z += dz * this.speed * dt;
   }
 }
