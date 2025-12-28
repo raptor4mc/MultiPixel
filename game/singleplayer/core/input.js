486a092fc8cc0cda0986c95ef925375b1320114c
Raptor4 <raptor4mc@gmail.com>
2025-12-28 14:42:21 +0100

    Update input.js

--- game/singleplayer/core/input.js
+++ game/singleplayer/core/input.js
@@ -1,12 +1,40 @@
 export class Input {
-  constructor() {
+  constructor(canvas = document.body) {
     this.keys = new Set();
 
+    this.mouse = {
+      dx: 0,
+      dy: 0,
+      sensitivity: 0.002
+    };
+
+    // Keyboard
     window.addEventListener("keydown", e => this.keys.add(e.code));
     window.addEventListener("keyup", e => this.keys.delete(e.code));
+
+    // Pointer lock
+    canvas.addEventListener("click", () => {
+      canvas.requestPointerLock();
+    });
+
+    // Mouse movement
+    window.addEventListener("mousemove", e => {
+      if (document.pointerLockElement === canvas) {
+        this.mouse.dx += e.movementX;
+        this.mouse.dy += e.movementY;
+      }
+    });
   }
 
   isDown(key) {
     return this.keys.has(key);
+  }
+
+  consumeMouse() {
+    const dx = this.mouse.dx;
+    const dy = this.mouse.dy;
+    this.mouse.dx = 0;
+    this.mouse.dy = 0;
+    return { dx, dy };
   }
 }
