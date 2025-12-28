0a759137ef294127718df1001666c06f8644b375
Raptor4 <raptor4mc@gmail.com>
2025-12-28 23:33:48 +0100

    Update singleplayer.js

--- game/singleplayer/singleplayer.js
+++ game/singleplayer/singleplayer.js
@@ -1,4 +1,22 @@
+import { buildSettings } from "./logic/settings.js";
+import { validateSettings } from "./logic/validation.js";
+import { routeToEngine } from "./logic/router.js";
+
 import "./events/onGamemodeSelect.js";
 import "./events/onTerrainSelect.js";
 import "./events/onSeedInput.js";
 import "./events/onCreateWorld.js";
+import "./events/onLoadWorld.js";
+
+window.appState = {
+  worldName: "",
+  seed: null,
+  gamemode: "survival",
+  terrain: "normal"
+};
+
+document.getElementById("createWorld").onclick = () => {
+  const settings = buildSettings(window.appState);
+  validateSettings(settings);
+  routeToEngine(settings);
+};
