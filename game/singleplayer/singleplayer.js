6814c86d92b5e1f61bebf12dd5fb961d84211e9c
Raptor4 <raptor4mc@gmail.com>
2025-12-29 10:37:20 +0100

    Update singleplayer.js

--- game/singleplayer/singleplayer.js
+++ game/singleplayer/singleplayer.js
@@ -1,12 +1,31 @@
-import "./events/onGamemodeSelect.js";
-import "./events/onTerrainSelect.js";
-import "./events/onSeedInput.js";
-import "./events/onCreateWorld.js";
-import "./events/onLoadWorld.js";
+import { SETTINGS, PLAYER } from "./logic/settings.js";
+import { BLOCKS, BLOCK_IDS } from "./logic/presets.js";
+import { generateChunkData } from "./world/generator/normal.js";
+import { startGame } from "./logic/router.js";
 
-window.appState = {
-  worldName: "",
-  seedText: "",
-  gamemode: "survival",
-  terrain: "normal"
-};
+let scene, camera, renderer, simplex;
+
+function init() {
+  scene = new THREE.Scene();
+  simplex = new SimplexNoise();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  animate();
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+startGame(init);
