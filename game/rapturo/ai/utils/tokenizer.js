0597cdde2e10a8553fe44c174b03520648cfc44c
Raptor4 <raptor4mc@gmail.com>
2026-01-12 00:42:40 +0100

    Update tokenizer.js

--- game/rapturo/ai/utils/tokenizer.js
+++ game/rapturo/ai/utils/tokenizer.js
@@ -1,1 +1,23 @@
+export class Tokenizer {
+    constructor(vocab) {
+        this.vocab = vocab;
+        // Create a reverse lookup (ID -> Word/Char)
+        this.decoder = Object.keys(vocab).reduce((acc, key) => {
+            acc[vocab[key]] = key;
+            return acc;
+        }, {});
+    }
 
+    encode(text) {
+        // Simple character or whitespace splitting - adjust based on your training
+        // This is a basic example. 
+        const tokens = text.split('').map(char => {
+            return this.vocab[char] || this.vocab['<UNK>'] || 0;
+        });
+        return tokens;
+    }
+
+    decode(ids) {
+        return ids.map(id => this.decoder[id] || '').join('');
+    }
+}
