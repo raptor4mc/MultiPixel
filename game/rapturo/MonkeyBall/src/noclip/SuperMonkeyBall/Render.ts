a7d800a7d7be7fe1a054826bd78e902c2150717b
Raptor4 <raptor4mc@gmail.com>
2026-01-30 16:45:22 +0100

    Create Render.ts

---
+++ game/rapturo/MonkeyBall/src/noclip/SuperMonkeyBall/Render.ts
@@ -0,0 +1,29 @@
+import type { vec3 } from 'gl-matrix';
+import type { GfxDevice } from '../gfx/platform/GfxPlatform.js';
+import type { GfxRenderInstList, GfxRenderInstManager } from '../gfx/render/GfxRenderInstManager.js';
+
+export type RenderContext = {
+  device: GfxDevice;
+  renderInstManager: GfxRenderInstManager;
+  viewerInput: {
+    camera: any;
+    time: number;
+    deltaTime: number;
+    backbufferWidth: number;
+    backbufferHeight: number;
+    onscreenTexture: any;
+    antialiasingMode: number;
+    mouseLocation: { mouseX: number; mouseY: number };
+    debugConsole: { addInfoLine: (line: string) => void };
+  };
+  opaqueInstList: GfxRenderInstList;
+  translucentInstList: GfxRenderInstList;
+  bgOpaqueInstList?: GfxRenderInstList;
+  bgTranslucentInstList?: GfxRenderInstList;
+  forceAlphaWrite?: boolean;
+  skipMirrorModels?: boolean;
+  skipStageTilt?: boolean;
+  mirrorCapture?: boolean;
+  mirrorPlanePoint?: vec3;
+  mirrorPlaneNormal?: vec3;
+};
