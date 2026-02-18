a290cd5c65fbcf9f6d0b85e4233470c6dc1affd8
Raptor4 <raptor4mc@gmail.com>
2026-02-19 00:59:48 +0100

    Update pickaxes.js

--- game/singleplayer/tools/pickaxes.js
+++ game/singleplayer/tools/pickaxes.js
@@ -32,6 +32,7 @@
     if (blockId === 30) return { id: 31, count: 1 };     // Steel ore -> iron
     if (blockId === 1) return { id: 2, count: 1};
     if (blockId === 35) return { id: 38, count: 3};
+    if (blockId === 40) return { id: 42, count: 1};
     return { id: blockId, count: 1 };
   }
 
