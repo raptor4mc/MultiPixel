2c90b4959290fb0f88e5c5e7ed5e6cfb9c4a59cb
Raptor4 <raptor4mc@gmail.com>
2026-02-18 10:57:53 +0100

    Update biome.js

--- game/singleplayer/terrain/plains/biome.js
+++ game/singleplayer/terrain/plains/biome.js
@@ -1,27 +1,11 @@
 (function () {
-
   const PlainsTerrain = {
     isBiome({ humidityNoise, mountainNoise }) {
       return humidityNoise > -0.2 && mountainNoise < 0.55;
     },
-
-    getHeight({ BASE_LAND_Y, perlin, x, z }) {
-      // Get the shared base terrain for smooth transitions
-      const base = getBaseTerrain(x, z, perlin);
-
-      // Base elevation from continental mask
-      const baseHeight = BASE_LAND_Y + base.continentalMask * 6;
-
-      // Gentle rolling hills
-      const hills = base.terrainNoise * 2.0;
-
-      // Small jaggedness from erosion
-      const jagged = base.erosionNoise * 0.5;
-
-      return baseHeight + hills + jagged;
+    getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise }) {
+      return BASE_LAND_Y + continentalMask * 7 + terrainNoise * 2.3 - erosionNoise * 1.8;
     },
   };
-
   window.PlainsTerrain = PlainsTerrain;
-
 })();
