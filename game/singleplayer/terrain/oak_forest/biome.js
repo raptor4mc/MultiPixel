7461f1587060657085d2ac1efc41541e8b75faff
Raptor4 <raptor4mc@gmail.com>
2026-02-18 10:59:02 +0100

    Update biome.js

--- game/singleplayer/terrain/oak_forest/biome.js
+++ game/singleplayer/terrain/oak_forest/biome.js
@@ -1,28 +1,12 @@
 (function () {
-
   const OakForestTerrain = {
     isBiome({ detailNoise, humidityNoise, distFromCenter, ISLAND_RADIUS }) {
       if (distFromCenter < ISLAND_RADIUS) return true;
       return detailNoise > -0.08 && humidityNoise > 0.05;
     },
-
-    getHeight({ BASE_LAND_Y, perlin, x, z }) {
-      // Get shared base terrain
-      const base = getBaseTerrain(x, z, perlin);
-
-      // Base elevation using continental mask
-      const baseHeight = BASE_LAND_Y + base.continentalMask * 10;
-
-      // Gentle rolling hills
-      const hills = base.terrainNoise * 5.5;
-
-      // Small jaggedness using erosion
-      const jagged = Math.pow(Math.max(0, base.erosionNoise), 1.8) * 3;
-
-      return baseHeight + hills + jagged;
+    getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise }) {
+      return BASE_LAND_Y + continentalMask * 10 + terrainNoise * 5.8 - erosionNoise * 1.2;
     },
   };
-
   window.OakForestTerrain = OakForestTerrain;
-
 })();
