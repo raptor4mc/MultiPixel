a8f3d15fc36fc6e29056d33931fb9396d2ca90ac
Raptor4 <raptor4mc@gmail.com>
2026-02-18 11:12:01 +0100

    Update biome.js

--- game/singleplayer/terrain/plains/biome.js
+++ game/singleplayer/terrain/plains/biome.js
@@ -1,11 +1,19 @@
 (function () {
   const PlainsTerrain = {
     isBiome({ humidityNoise, mountainNoise }) {
+      // Plains: moderately humid, mostly flat
       return humidityNoise > -0.2 && mountainNoise < 0.55;
     },
+
     getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise }) {
-      return BASE_LAND_Y + continentalMask * 7 + terrainNoise * 2.3 - erosionNoise * 1.8;
+      // Smoother height variation using slight weighted adjustments
+      const continentalHeight = continentalMask * 7;
+      const terrainVariation = terrainNoise * 2.5;  // slightly more variation
+      const erosionEffect = erosionNoise * 1.8;     // same as before
+
+      return BASE_LAND_Y + continentalHeight + terrainVariation - erosionEffect;
     },
   };
+
   window.PlainsTerrain = PlainsTerrain;
 })();
