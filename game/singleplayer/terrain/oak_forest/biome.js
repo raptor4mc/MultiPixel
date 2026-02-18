1e31edef0cc300510626cb0bd56bddd741b36717
Raptor4 <raptor4mc@gmail.com>
2026-02-18 07:38:21 +0100

    Update biome.js

--- game/singleplayer/terrain/oak_forest/biome.js
+++ game/singleplayer/terrain/oak_forest/biome.js
@@ -1,12 +1,13 @@
-(function () {
-  const OakForestTerrain = {
-    isBiome({ detailNoise, humidityNoise, distFromCenter, ISLAND_RADIUS }) {
-      if (distFromCenter < ISLAND_RADIUS) return true;
-      return detailNoise > -0.08 && humidityNoise > 0.05;
-    },
-    getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise }) {
-      return BASE_LAND_Y + continentalMask * 10 + terrainNoise * 5.8 - erosionNoise * 1.2;
-    },
-  };
-  window.OakForestTerrain = OakForestTerrain;
-})();
+const OakForestTerrain = {
+  isBiome({ detailNoise, humidityNoise, distFromCenter, ISLAND_RADIUS }) {
+    if (distFromCenter < ISLAND_RADIUS) return true;
+    return detailNoise > -0.08 && humidityNoise > 0.05;
+  },
+  getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise, perlin, x, z }) {
+    const base = BASE_LAND_Y + continentalMask * 10;
+    const hills = terrainNoise * 5.5;
+    const jagged = Math.pow(Math.max(0, erosionNoise), 1.8) * 3;
+    return base + hills + jagged;
+  },
+};
+window.OakForestTerrain = OakForestTerrain;
