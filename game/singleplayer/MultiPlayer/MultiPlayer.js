5dd4c43ea5b96873f757aa70ad2b36731e4fabaf
Raptor4 <raptor4mc@gmail.com>
2026-02-27 20:25:25 +0100

    Create MultiPlayer.js

---
+++ game/singleplayer/MultiPlayer/MultiPlayer.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBC30_6k8TMjtYhOnkbOuYEr2_iJckM8nw",
+  authDomain: "multipixel-multiplayer.firebaseapp.com",
+  projectId: "multipixel-multiplayer",
+  storageBucket: "multipixel-multiplayer.firebasestorage.app",
+  messagingSenderId: "1048769081726",
+  appId: "1:1048769081726:web:4a246c7b180edc1c6fc32f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
