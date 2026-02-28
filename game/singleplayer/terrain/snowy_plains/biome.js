a3bc3f4cd89be48fc49abdaf6c601347beb3d734
Raptor4 <raptor4mc@gmail.com>
2026-02-28 19:02:02 +0100

    Fix server data requirements document filename and content

--- game/singleplayer/terrain/snowy_plains/biome.js
+++ game/singleplayer/terrain/snowy_plains/biome.js
@@ -6,6 +6,17 @@
     getHeight({ BASE_LAND_Y, continentalMask, terrainNoise, erosionNoise }) {
       return BASE_LAND_Y + continentalMask * 6.2 + terrainNoise * 1.9 - erosionNoise * 1.1;
     },
+    structures: {
+      igloo: {
+        spawnChancePerChunk: 0.11,
+        validSurfaceBlockId: 15
+      }
+    },
+    shouldSpawnIgloo({ cx, cz, hashRand2D, spawnChance }) {
+      const chance = typeof spawnChance === 'number' ? spawnChance : 0.11;
+      const roll = hashRand2D(cx * 19 + 7, cz * 19 - 13, 905);
+      return roll < chance;
+    }
   };
 
   window.SnowyPlainsTerrain = SnowyPlainsTerrain;
